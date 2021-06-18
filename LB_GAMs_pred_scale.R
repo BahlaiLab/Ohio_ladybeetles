43acde9c067996c4237bdbcd287f3d10e7a1fc7d
Christie Bahlai <cbahlai@kent.edu>
2021-06-18 17:29:47 -0400

    prototype predictions figure built!

--- LB_GAMs_pred_scale.R
+++ LB_GAMs_pred_scale.R
@@ -45,7 +45,7 @@ newPI <- with(lb_all2,
                           Totalcount= 500, 
                           lon=mean(lon), 
                           lat=mean(lat), 
-                          Propinvasive=seq(min(Propinvasive), max(Propinvasive), length = 100),
+                          Propinvasive=seq(min(Propinvasive), max(Propinvasive)-0.05, length = 100),
                           Agriculture=mean(Agriculture, na.rm=T), 
                           Forest=mean(Forest, na.rm=T), 
                           Developed=mean(Developed, na.rm=T)))
@@ -99,19 +99,78 @@ cmac.pred<-cbind(newd,cmac.pred)
 cmac.pred$lower<-cmac.pred$fit-2*cmac.pred$se.fit
 cmac.pred$upper<-cmac.pred$fit+2*cmac.pred$se.fit
 
-jitter<-position_jitter(width = 0.1, height = 0.02)
 
 cmac.pred.decade<-ggplot(data=cmac.pred, aes(Decade, fit))+
-  geom_point(data=lb_all2, aes(Decade, Coleomegilla.maculata), position = jitter, pch=21, size=1, fill="lightgrey")+
-  geom_ribbon(aes(ymin=lower, ymax=upper), fill='paleturquoise', alpha=0.6)+
+  geom_ribbon(aes(ymin=lower, ymax=upper), fill="grey19", alpha=0.6)+
   geom_line()+
   theme_classic()+
   xlim(1929, 2011)+
-  xlab(NULL)+ylab("")+
-  scale_y_continuous(trans='pseudo_log',limits=c(-0.1, NA))
+  xlab(expression(paste("Year")))+ylab("Predicted captures")+
+  scale_y_continuous(limits=c(-0.1, NA))
 cmac.pred.decade
 
+#lat
+cmac.pred<-predict.gam(cmac.gam4, newlat, se.fit = T, type="link")
+cmac.pred<-cbind(newlat,cmac.pred)
+cmac.pred$lower<-cmac.pred$fit-2*cmac.pred$se.fit
+cmac.pred$upper<-cmac.pred$fit+2*cmac.pred$se.fit
 
+cmac.pred.lat<-ggplot(data=cmac.pred, aes(lat, fit))+
+  geom_ribbon(aes(ymin=lower, ymax=upper), fill="grey", alpha=0.6)+
+  geom_line(col="gray28")+
+  theme_classic()+
+  xlab(expression(paste("Latitude")))+ylab("Predicted captures")+
+  scale_y_continuous(limits=c(-0.1, NA))
+cmac.pred.lat
+
+
+#prop invasive
+
+cmac.pred<-predict.gam(cmac.gam4, newPI, se.fit = T, type="link")
+cmac.pred<-cbind(newPI,cmac.pred)
+cmac.pred$lower<-cmac.pred$fit-2*cmac.pred$se.fit
+cmac.pred$upper<-cmac.pred$fit+2*cmac.pred$se.fit
+
+cmac.pred.pi<-ggplot(data=cmac.pred, aes(Propinvasive, fit))+
+  geom_ribbon(aes(ymin=lower, ymax=upper), fill="plum1", alpha=0.6)+
+  geom_line(col="darkmagenta")+
+  theme_classic()+
+  xlab(expression(paste("Proportion invasive")))+ylab("Predicted captures")+
+  scale_y_continuous(limits=c(-0.5, NA))
+cmac.pred.pi
+
+
+#agriculture
+
+cmac.pred<-predict.gam(cmac.gam4, newAgriculture, se.fit = T, type="link")
+cmac.pred<-cbind(newAgriculture,cmac.pred)
+cmac.pred$lower<-cmac.pred$fit-2*cmac.pred$se.fit
+cmac.pred$upper<-cmac.pred$fit+2*cmac.pred$se.fit
+
+cmac.pred.agriculture<-ggplot(data=cmac.pred, aes(Agriculture, fit))+
+  geom_ribbon(aes(ymin=lower, ymax=upper), fill="darkolivegreen1", alpha=0.6)+
+  geom_line(col="darkolivegreen")+
+  theme_classic()+
+  xlab(expression(paste("% Agriculture cover")))+ylab("Predicted captures")+
+  scale_y_continuous(limits=c(-0.1, NA))
+cmac.pred.agriculture
+
+
+#create graphical objects for placeholders in plots
+noeffect<-text_grob(paste("No effect"), color="black")
+notpresent<-text_grob(paste("Not present"), color="black")
+
+cmac.predictions<-plot_grid(cmac.pred.decade, noeffect, cmac.pred.lat, 
+                        cmac.pred.pi, noeffect, noeffect,
+                        cmac.pred.agriculture, noeffect, noeffect,
+                        ncol=3, rel_widths=c(1,1,1), labels=c('A', 'B', 'C', 
+                                                              'D', 'E', 'F',
+                                                              'G', 'H', 'I'))
+cmac.predictions
+
+pdf("plots/cmac_predictions.pdf", height=9, width=9)
+grid.draw(cmac.predictions)
+dev.off()
 
 
 
