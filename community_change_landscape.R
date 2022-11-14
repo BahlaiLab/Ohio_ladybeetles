f8b378abee56a2bc51976a5f5d1f45299dc477d4
Christie Bahlai <cbahlai@kent.edu>
2022-11-14 17:34:49 -0500

    build the species nmdses

--- community_change_landscape.R
+++ community_change_landscape.R
@@ -8,7 +8,10 @@ LULC.raw.wide<-dcast(LULC, Year+County~Class, value.var="Percentage")
 #let's also use the raw ladybeetle collections for this as well- first need to create a classifier 
 #so we can map these onto the timepoints we have solid landcover data for
 
-lb_raw$YearGroup<-cut(lb_raw$Year, breaks=c(-Inf, 1938, 1970, 1992, 2016, Inf), labels=c("Before 1938","1939-1970","1971-1992","1993-2016","After 2017"))
+#first just cull out data after 2016
+lb_raw<-lb_raw[which(lb_raw$Year<2017),]
+
+lb_raw$YearGroup<-cut(lb_raw$Year, breaks=c(-Inf, 1938, 1970, 1992,  Inf), labels=c("1938","1970","1992","2016"))
 
 
 lb.wide.yeargroups<-dcast(lb_raw, YearGroup+County~Name, length)
@@ -17,10 +20,10 @@ lb.wide.yeargroups<-dcast(lb_raw, YearGroup+County~Name, length)
 
 cutpoint<-rowSums(lb.wide.yeargroups[3:30])
 lb.wide.yeargroups<-lb.wide.yeargroups[which(cutpoint>2), ]
-#also very little data after 2017 so let's just cut that
-lb.wide.yeargroups<-lb.wide.yeargroups[which(lb.wide.yeargroups$YearGroup!="After 2017"), ]
 
-lb.wide.yeargroups.natives<-lb.wide.yeargroups[,-c(10,13,16,22,29)]
+
+
+lb.wide.yeargroups.natives<-lb.wide.yeargroups[,-c(11,14,17,23,29)]
 #gotta cull out county/groups with insufficient observations to NMDS again!
 
 cutpoint1<-rowSums(lb.wide.yeargroups.natives[3:25])
@@ -44,20 +47,20 @@ lb.ord<-metaMDS(lb.wide.yeargroups[3:30])
 lb.ord
 
 plot(lb.ord, disp='sites', type="n")
-points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="Before 1938"), pch=19, col="pink")
-points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="1939-1970"), pch=19, col="orange")
-points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="1971-1992"), pch=19, col="green")
-points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="1993-2016"), pch=19, col="blue")
+points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="1938"), pch=19, col="pink")
+points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="1970"), pch=19, col="orange")
+points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="1992"), pch=19, col="green")
+points(lb.ord, display="sites", select=which(lb.wide.yeargroups$YearGroup=="2016"), pch=19, col="blue")
 ordiellipse(lb.ord, lb.wide.yeargroups$YearGroup, draw="polygon", col=c("pink", "orange", "green", "blue"), kind="se", conf=0.999, label=TRUE)
 
 lb.ord.n<-metaMDS(lb.wide.yeargroups.natives[3:25])
 lb.ord.n
 
 plot(lb.ord.n, disp='sites', type="n")
-points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="Before 1938"), pch=19, col="pink")
-points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="1939-1970"), pch=19, col="orange")
-points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="1971-1992"), pch=19, col="green")
-points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="1993-2016"), pch=19, col="blue")
+points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="1938"), pch=19, col="pink")
+points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="1970"), pch=19, col="orange")
+points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="1992"), pch=19, col="green")
+points(lb.ord.n, display="sites", select=which(lb.wide.yeargroups.natives$YearGroup=="2016"), pch=19, col="blue")
 ordiellipse(lb.ord.n, lb.wide.yeargroups.natives$YearGroup, draw="polygon", col=c("pink", "orange", "green", "blue"), kind="se", conf=0.999, label=TRUE)
 
 
@@ -77,7 +80,7 @@ landscape.centroids$landuse <- rownames(landscape.centroids)
 fudgexl<-c(0.05, 0.06, 0.015, 0.07)#jitter the vector labels a bit
 fudgeyl<-c(0, 0.03, 0.07, 0.02)
 
-landnmds<-gg_ordiplot(landscape.ord, groups= LULC.raw.wide$Year, kind="se", conf=0.99, pt.size=-1)
+landnmds<-gg_ordiplot(landscape.ord, groups= LULC.raw.wide$Year, kind="se", conf=0.99, pt.size=-1, plot=F)
 
 gglandnmds<-landnmds$plot+  
   geom_point(data=landnmds$df_ord, aes(x=x,y=y, color=Group, shape=Group))+
@@ -100,4 +103,62 @@ gglandnmds<-landnmds$plot+
   labs(x="NMDS1", y="NMDS2")
 
 gglandnmds
+
+#all lbs
+
+
+fudgexa<-c(0.2,-0.1,0,0)#jitter the vector labels a bit
+fudgeya<-c(-0.3,-0.32,0.28,-0.23)
+
+lbnmds<-gg_ordiplot(lb.ord, groups=lb.wide.yeargroups$YearGroup, kind="se", conf=0.99, pt.size=-1, plot=F)
+
+gglbnmds<-lbnmds$plot+  
+  geom_point(data=lbnmds$df_ord, aes(x=x,y=y, color=Group, shape=Group))+
+  scale_colour_manual(values=c("1938" = "darkred", "1970" = "darkorange", "1992"= "yellow3", "2016"="darkgreen"), 
+                      labels=c("Before 1938", "1939-1970", "1971-1992", "1993-2016"), title("Year"))+
+  scale_fill_manual(values=c("1938" = "darkred", "1970" = "darkorange", "1992"= "yellow3", "2016"="darkgreen"), 
+                    labels=c("1938", "1970", "1992", "2016"), title("Year"))+
+  scale_shape_manual(values=c("1938" = 1, "1970" = 2, "1992"= 3, "2016"=4), 
+                     labels=c("Before 1938", "1939-1970", "1971-1992", "1993-2016"), title("Year"))+
+  geom_polygon(data = lbnmds$df_ellipse, aes(x = x, y = y,  fill=Group), show.legend = FALSE, color="black", alpha =0.25)+
+  geom_label(data = lbnmds$df_mean.ord, aes(x = x+fudgexa, y = y+fudgeya, label = Group, color = Group), 
+             show.legend = FALSE, fill="white", color="black", alpha=0.9)+
+  geom_path(data = lbnmds$df_mean.ord, aes(x = x, y = y), 
+            show.legend = FALSE, color="black")+
+  geom_point(data = lbnmds$df_mean.ord, aes(x = x, y = y), 
+             show.legend = FALSE, color="black", pch=16)+
+  theme_classic()+
+  labs(x="NMDS1", y="NMDS2")
+
+gglbnmds
+
+
+#native lbs only
+
+
+fudgexn<-c(0.25,-0.1,0,0.1)#jitter the vector labels a bit
+fudgeyn<-c(-0.24,-0.32,0.28,-0.25)
+
+lbnnmds<-gg_ordiplot(lb.ord.n, groups=lb.wide.yeargroups.natives$YearGroup, kind="se", conf=0.99, pt.size=-1, plot=F)
+
+gglbnnmds<-lbnnmds$plot+  
+  geom_point(data=lbnnmds$df_ord, aes(x=x,y=y, color=Group, shape=Group))+
+  scale_colour_manual(values=c("1938" = "darkred", "1970" = "darkorange", "1992"= "yellow3", "2016"="darkgreen"), 
+                      labels=c("Before 1938", "1939-1970", "1971-1992", "1993-2016"), title("Year"))+
+  scale_fill_manual(values=c("1938" = "darkred", "1970" = "darkorange", "1992"= "yellow3", "2016"="darkgreen"), 
+                    labels=c("1938", "1970", "1992", "2016"), title("Year"))+
+  scale_shape_manual(values=c("1938" = 1, "1970" = 2, "1992"= 3, "2016"=4), 
+                     labels=c("Before 1938", "1939-1970", "1971-1992", "1993-2016"), title("Year"))+
+  geom_polygon(data = lbnnmds$df_ellipse, aes(x = x, y = y,  fill=Group), show.legend = FALSE, color="black", alpha =0.25)+
+  geom_label(data = lbnnmds$df_mean.ord, aes(x = x+fudgexn, y = y+fudgeyn, label = Group, color = Group), 
+             show.legend = FALSE, fill="white", color="black", alpha=0.9)+
+  geom_path(data = lbnnmds$df_mean.ord, aes(x = x, y = y), 
+            show.legend = FALSE, color="black")+
+  geom_point(data = lbnnmds$df_mean.ord, aes(x = x, y = y), 
+             show.legend = FALSE, color="black", pch=16)+
+  theme_classic()+
+  labs(x="NMDS1", y="NMDS2")
+
+gglbnnmds
+
 
